4e97ffcc6e56f4bd2aece0f6fcad7e15e4e084a2
literallyanything <hunterbaker@me.com>
2023-11-17 18:47:07 -0500

    Fix indent

--- setup.py
+++ setup.py
@@ -3,23 +3,23 @@ from setuptools import find_packages, setup
 package_name = 'avr_vmc_2023_bdu'
 
 setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Hunter Baker',
-    maintainer_email='hunterbaker@me.com',
-    description='A node to trigger the ball dropper with the correct timings',
-    license='LGPL-3.0-only',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'bdu_trigger_node = avr_vmc_2023_bdu.bdu_trigger:main',
+        name=package_name,
+        version='0.0.0',
+        packages=find_packages(exclude=['test']),
+        data_files=[
+            ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+            ('share/' + package_name, ['package.xml']),
         ],
-    },
+        install_requires=['setuptools'],
+        zip_safe=True,
+        maintainer='Hunter Baker',
+        maintainer_email='hunterbaker@me.com',
+        description='A node to trigger the ball dropper with the correct timings',
+        license='LGPL-3.0-only',
+        tests_require=['pytest'],
+        entry_points={
+            'console_scripts': [
+                'bdu_trigger_node = avr_vmc_2023_bdu.bdu_trigger:main',
+            ],
+        },
 )
