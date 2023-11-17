d08834384b2dec9a36158eb9d7ada59a3b42fcfe
literallyanything <hunterbaker@me.com>
2023-11-17 18:48:33 -0500

    Add support for multi-stage dropping

--- avr_vmc_2023_bdu/bdu_trigger.py
+++ avr_vmc_2023_bdu/bdu_trigger.py
@@ -12,116 +12,101 @@ class BDUTriggerNode(Node):
         super().__init__('bdu_trigger', namespace='bdu')
 
         self.declare_parameter('hold_duration', 1.0)
-        self.declare_parameter('max_value', 255)
+        self.declare_parameter('stage', 50)
+        self.declare_parameter('stage_count', 4)
         self.declare_parameter('min_value', 0)
         self.declare_parameter('servo_num', 0)
 
         self.hold_duration = self.get_parameter('hold_duration').value
-        self.max_value = int(self.get_parameter('max_value').value)
+        self.stage = int(self.get_parameter('stage').value)
+        self.stage_count = int(self.get_parameter('stage_count').value)
         self.min_value = int(self.get_parameter('min_value').value)
         self.servo_num = int(self.get_parameter('servo_num').value)
 
-        # noinspection PyTypeChecker
+        self.full_trigger_service = self.create_service(
+                Trigger,
+                'full_trigger',
+                self.full_trigger
+        )
         self.trigger_service = self.create_service(
                 Trigger,
                 'trigger',
                 self.trigger
+        )
+        self.reset_service = self.create_service(
+                Trigger,
+                'reset',
+                self.reset
         )
         self.set_servo_client = self.create_client(
                 SetServo,
                 '/servo/set_position',
         )
 
-        # self.finish_timer = self.create_timer(
-        #         callback=self.finish,
-        #         timer_period_sec=self.hold_duration,
-        # )
-        # self.finish_timer.cancel()
+        self.finish_timer = self.create_timer(
+                callback=self.finish,
+                timer_period_sec=self.hold_duration,
+        )
+        self.finish_timer.cancel()
 
-        setup_done = False
-        while not setup_done:
-            self.get_logger().error('No response from servo service. Retrying...', skip_first=True)
-            future = self.set_servo(False)
-            rclpy.spin_until_future_complete(self, future, timeout_sec=2)
-            setup_done = True
-            # setup_done = future.done()
-            # if not setup_done:
-            #     future.cancel()
+        self.get_logger().error('Waiting for servo set_position service...')
+        self.set_servo_client.wait_for_service()
+        future = self.set_servo(False)
+        rclpy.spin_until_future_complete(self, future, timeout_sec=5)
 
-    def trigger(self, _: Trigger.Request, response: Trigger.Response) -> Trigger.Response:
-        Thread(target=self.worker, daemon=True).start()
+        self.prev_future: rclpy.Future = rclpy.Future()
+        self.current_stage: int = 0
+
+    def full_trigger(self, _: Trigger.Request, response: Trigger.Response) -> Trigger.Response:
+        self.prev_future.cancel()
+        self.current_stage = self.stage_count + 1
+
+        future = self.set_servo(self.current_stage)
+        self.prev_future = future
+        future.add_done_callback(lambda: self.finish_timer.reset())
+
+        response.message = 'full triggered. Resetting...'
+        response.success = True
         return response
 
-    def worker(self) -> None:
-        self.set_servo(True)
-        sleep(self.hold_duration + 0.5)
-        self.set_servo(False)
-    #     """
-    #     The trigger service callback.
-    #     Calls the servo service to drop, then starts a timer to finish the drop.
-    #
-    #     :param _: The request object.
-    #     This parameter is ignored as it has no data.
-    #     :param response: The response object to be updated based on the servo service response.
-    #
-    #     :return: The updated response object.
-    #     """
-    #     future_done = Event()
-    #     future = self.set_servo(True)
-    #     # future.add_done_callback(lambda: future_done.set())
-    #     future_done.set()
-    #
-    #     if future_done.wait(1):
-    #         # servo_response = future.result()
-    #
-    #         # response.success = servo_response.success
-    #         # response.message = 'Success' if servo_response.success else 'Failed'
-    #
-    #         self.finish_timer.reset()
-    #         self.get_logger().info('Reset the timer')
-    #     else:
-    #         # future.cancel()
-    #
-    #         response.success = False
-    #         response.message = 'No response from servo service'
-    #         self.get_logger().warn('Failed to call service to start drop')
-    #
-    #     return response
-    #
-    # def finish(self) -> None:
-    #     """
-    #     This method is used to finish the task by setting the servo to False, canceling the finish timer,
-    #     and handling any timeouts that may occur.
-    #     """
-    #     future_done = Event()
-    #     future = self.set_servo(False)
-    #     # future.add_done_callback(lambda: future_done.set())
-    #     future_done.set()
-    #
-    #     if future_done.wait(1):
-    #         # servo_response = future.result()
-    #
-    #         # if not servo_response.success:
-    #         #     self.get_logger().warn('Failed set servo to finish drop. Retrying...')
-    #         # else:
-    #         self.finish_timer.cancel()
-    #         self.get_logger().info('Cancel the timer')
-    #     else:
-    #         # future.cancel()
-    #
-    #         self.get_logger().warn('Failed to call service to finish drop. Retrying...')
+    def trigger(self, _: Trigger.Request, response: Trigger.Response) -> Trigger.Response:
+        self.prev_future.cancel()
+        self.current_stage = (self.current_stage + 1) % (self.stage_count + 1)
 
-    def set_servo(self, state: bool) -> rclpy.Future:
-        """
-        Sends a request to set the servo position based on the provided state.
+        future = self.set_servo(self.current_stage)
+        self.prev_future = future
+        if self.current_stage == self.stage_count:
+            future.add_done_callback(lambda: self.finish_timer.reset())
+            response.message = 'Last stage triggered. Resetting...'
+        else:
+            response.message = 'Triggered stage'
 
-        :param state: A boolean indicating whether the servo should be set to the maximum value or the minimum value.
+        response.success = True
+        return response
 
-        :return: A future that represents the asynchronous result of the request.
-        """
+    def reset(self, _: Trigger.Request, response: Trigger.Response) -> Trigger.Response:
+        self.prev_future.cancel()
+        self.finish_timer.cancel()
+
+        self.current_stage = 0
+
+        self.prev_future.cancel()
+        self.prev_future = self.set_servo(0)
+
+        response.success = True
+        response.message = 'Reset'
+        return response
+
+    def finish(self) -> None:
+        self.prev_future.cancel()
+        self.prev_future = self.set_servo(0)
+
+    def set_servo(self, state: int) -> rclpy.Future:
+        state = min(self.stage_count + 1, max(0, state))
+
         servo_request = SetServo.Request()
         servo_request.servo_num = self.servo_num
-        servo_request.value = self.max_value if state else self.min_value
+        servo_request.value = (self.stage_count * state) + self.min_value
         return self.set_servo_client.call_async(servo_request)
 
 
